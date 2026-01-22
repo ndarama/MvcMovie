301a522f578590112040fbf0c480c6fd09f92baa
ndaramai <ndaramartwork@gmail.com>
2026-01-23 01:56:05 +0200

    Additional and new seed data

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9266c7f7d8b4d785061da01bd2cdd5a780047d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01f72f43a4cd3cafdda9acbc0a3e1d7302feafc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
