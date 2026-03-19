59789c0b44cbeaa1faf9aa4d11ac9f3dcee16fa3
ndaramai <ndaramartwork@gmail.com>
2026-03-19 22:30:31 +0200

    Remove publish folder and add .gitignore

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d341a6541efa8ae9755cd68b73a89e8a8b6e6dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b653747c0b8bd5d67fa04e3bad6faa54aab605cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
