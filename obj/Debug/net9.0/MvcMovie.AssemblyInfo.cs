4a0518d180751e4f7c370ad21c8ea97173ca953d
ndaramai <ndaramartwork@gmail.com>
2026-03-19 21:45:20 +0200

    build successful

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9016ac197304f1e06f97e33b0f61fae9276a5802")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c16947d94b475d38f27b682a5ba521bb7d55fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
